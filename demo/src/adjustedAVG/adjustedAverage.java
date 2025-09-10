c2729a73291c3b091a7e940fbcecb28754c51b4e
SilverSun000 <williamcahill78@gmail.com>
2025-09-09 20:18:26 -0500

    had to pack up at work

--- demo/src/adjustedAVG/adjustedAverage.java
+++ demo/src/adjustedAVG/adjustedAverage.java
@@ -1,3 +1,17 @@
+package adjustedAVG;
+
 public class adjustedAverage {
-    // insert cool code here
+    public static double findAverage(int[] nums) {
+        // define length to avoid checking in loop
+        int length = nums.length;
+        int total;
+
+        // loop through given arr
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        
+        //return adjusted average
+        return total / length;
+    }
 }
